6ef81f6fda368cb851bb25d2536caea053278210
Paris Shirley <shirleyp1097@gmail.com>
2021-07-15 11:46:24 -0500

    complete for_loops and while loop exercises

--- js/while.js
+++ js/while.js
@@ -1,21 +1,24 @@
-// var i = 2;
-// while (i <= 65536) {
-//     console.log(i);
-//     i*=2;
-// }
+var i = 2;
+while (i <= 65536) {
+    console.log(i);
+    i*=2;
+}
+
+
+
+
 var conesLeft = Math.floor(Math.random() * 50) + 50;
 var conesSold = 0;
 
 do {
     var nextOrder = Math.floor(Math.random() * 5) + 1;
-    if (conesLeft > nextOrder) {
-        conesLeft -= nextOrder;
-        conesSold += nextOrder;
-        console.log(`${conesSold} cones sold`)
-        console.log(`${conesLeft} cones left`)
+    if (conesLeft < nextOrder) {
+        console.log("Sorry I only have " + conesLeft + " cones left")
     } else {
-        console.log(`Cannot sell you ${nextOrder} cones I only have ${conesLeft}`);
+        conesLeft -= nextOrder;
+        console.log("There are " + conesLeft + " cones left");
     }
+} while (conesLeft > 0)
+console.log("Yay, I sold them all")
 
-} while (conesLeft !== 0);
 
