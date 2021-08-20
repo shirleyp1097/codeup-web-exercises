55720cd8f8f9ae027e0dbb6381d274019ac510d9
Paris Shirley <shirleyp1097@gmail.com>
2021-08-20 16:49:43 -0500

    complete weather-map

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -47,7 +47,7 @@ function reverseGeocode(coordinates, token) {
             return res.json();
         })
         // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
-            return data.features[0].place_name;
-        });
+        // .then(function(data) {
+        //     return data.features[0].place_name;
+        // });
 }
