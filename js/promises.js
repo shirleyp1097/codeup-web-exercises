db6d0aceae38667d9c068a4cc62bbec1205183b5
Paris Shirley <shirleyp1097@gmail.com>
2021-09-07 13:29:57 -0500

    change weather-map to weather_map

--- js/promises.js
+++ js/promises.js
@@ -1,19 +1,26 @@
-function getGithubUsernames(username) {
-    fetch(`https://api.github.com/users/${String(username)}/events/public`, {headers: {'Authorization': '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'}})
-        .then(response => response.json())
-        .then(data =>
-            console.log(data[0]['created_at']))
-        .catch(error => console.log(error));
+// function getGithubUsernames(username) {
+//     fetch(`https://api.github.com/users/${String(username)}/events/public`, {headers: {'Authorization': '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'}})
+//         .then(response => response.json())
+//         .then(data =>
+//             console.log(data[0]['created_at']))
+//         .catch(error => console.log(error));
+//
+//
+// }
+//
+// // getGithubUsernames().then(users => {
+// //    users.forEach( userObj => {
+// //        console.log(userObj.login);
+// //    })
+// // });
+// getGithubUsernames('george-tudor');
 
 
-}
+const isDivisibleBy3 = n => n % 3 === 0;
 
-// getGithubUsernames().then(users => {
-//    users.forEach( userObj => {
-//        console.log(userObj.login);
-//    })
-// });
-getGithubUsernames('george-tudor');
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+console.log(numbers.filter(isDivisibleBy3));
+console.log(numbers);
 
 
